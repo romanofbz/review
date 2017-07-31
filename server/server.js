7224734cbc05a50ed351a864758fb6714ad6bd40
fabrizio <romanof74@gmail.com>
2017-07-31 16:41:36 +0200

    myDevBranch

---
+++ server/server.js
@@ -0,0 +1,105 @@
+// Set up
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var mongoose = require('mongoose');                     // mongoose for mongodb
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var cors = require('cors');
+const path = require('path');
+
+// Configuration
+//mongoose.connect('mongodb://localhost/reviewking');
+
+// Configuration
+mongoose.connect('mongodb://localhost/reviewking', (err,res) =>{
+    if (err) {
+        console.log('error');
+    } else {
+        console.log('connected to db' + res);
+    }
+}) 
+ 
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+app.use(cors());
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
+
+// Models
+var Review = mongoose.model('Review', {
+    title: String,
+    description: String,
+    rating: Number
+});
+
+app.use(express.static('/home/fabrizio/webdev/node/review/client/dist/'));
+
+    // Get reviews
+    app.get('/api/reviews', function(req, res) {
+ 
+        console.log("fetching reviews");
+ 
+        // use mongoose to get all reviews in the database
+        Review.find(function(err, reviews) {
+ 
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err)
+ 
+            res.json(reviews); // return all reviews in JSON format
+        });
+    });
+
+
+//----------- create review and send back all reviews after creation
+    app.post('/api/reviews', function(req, res) {
+ 
+        console.log("creating review");
+ 
+        // create a review, information comes from request from Ionic
+        Review.create({
+            title : req.body.title,
+            description : req.body.description,
+            rating: req.body.rating,
+            done : false
+        }, function(err, review) {
+            if (err)
+                res.send(err);
+ 
+            // get and return all the reviews after you create another
+            Review.find(function(err, reviews) {
+                if (err)
+                    res.send(err)
+                res.json(reviews);
+            });
+        });
+ 
+    });
+
+
+        // delete a review
+    app.delete('/api/reviews/:review_id', function(req, res) {
+        Review.remove({
+            _id : req.params.review_id
+        }, function(err, review) {
+ 
+        });
+    });
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.join('/home/fabrizio/webdev/node/review/client/dist/index.html' ));
+    });
+ 
+ 
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
